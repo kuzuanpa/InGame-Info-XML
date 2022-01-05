afbf9b1acc3c0b8260a3580b1c7d80df39b89b3d
bombcar <github@bombcar.com>
2022-01-05 00:28:43 -0600

    update and remove versioncheck (#7)

--- src/main/java/com/github/lunatrius/ingameinfo/proxy/CommonProxy.java
+++ src/main/java/com/github/lunatrius/ingameinfo/proxy/CommonProxy.java
@@ -1,6 +1,5 @@
 package com.github.lunatrius.ingameinfo.proxy;
 
-import com.github.lunatrius.core.version.VersionChecker;
 import com.github.lunatrius.ingameinfo.handler.ConfigurationHandler;
 import com.github.lunatrius.ingameinfo.network.PacketHandler;
 import com.github.lunatrius.ingameinfo.reference.Reference;
@@ -14,8 +13,6 @@ public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
         Reference.logger = event.getModLog();
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
-
-        VersionChecker.registerMod(event.getModMetadata(), Reference.FORGE);
     }
 
     public void init(FMLInitializationEvent event) {
