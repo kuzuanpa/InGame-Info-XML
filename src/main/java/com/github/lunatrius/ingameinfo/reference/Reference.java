daaac6d434a645401b5d86c25e39b24de2a97a6e
Johann Bernhardt <johann.bernhardt@tum.de>
2022-01-08 12:08:51 +0100

    Unified build script (#8)
    * Migrated to unified build script
    
    * Append to last

--- src/main/java/com/github/lunatrius/ingameinfo/reference/Reference.java
+++ src/main/java/com/github/lunatrius/ingameinfo/reference/Reference.java
@@ -6,9 +6,7 @@ import org.apache.logging.log4j.Logger;
 public class Reference {
     public static final String MODID = "InGameInfoXML";
     public static final String NAME = "InGame Info XML";
-    public static final String VERSION = "${version}";
-    public static final String FORGE = "${forgeversion}";
-    public static final String MINECRAFT = "${mcversion}";
+    public static final String VERSION = "GRADLETOKEN_MODNAME";
     public static final String PROXY_SERVER = "com.github.lunatrius.ingameinfo.proxy.ServerProxy";
     public static final String PROXY_CLIENT = "com.github.lunatrius.ingameinfo.proxy.ClientProxy";
     public static final String GUI_FACTORY = "com.github.lunatrius.ingameinfo.client.gui.GuiFactory";
