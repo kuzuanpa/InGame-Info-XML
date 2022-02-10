e16eb84abc93211bca052ad86aafcf8e35fd60c3
bombcar <github@bombcar.com>
2022-02-10 15:30:41 -0600

    the implication (#11)
    * the implication
    
    * fix bleh

--- src/main/java/com/github/lunatrius/ingameinfo/reference/Reference.java
+++ src/main/java/com/github/lunatrius/ingameinfo/reference/Reference.java
@@ -7,7 +7,6 @@ public class Reference {
     public static final String MODID = "InGameInfoXML";
     public static final String NAME = "InGame Info XML";
     public static final String VERSION = "GRADLETOKEN_VERSION";
-    public static final String DEPENDENCIES = "required-after:LunatriusCore;";
     public static final String PROXY_SERVER = "com.github.lunatrius.ingameinfo.proxy.ServerProxy";
     public static final String PROXY_CLIENT = "com.github.lunatrius.ingameinfo.proxy.ClientProxy";
     public static final String GUI_FACTORY = "com.github.lunatrius.ingameinfo.client.gui.GuiFactory";
